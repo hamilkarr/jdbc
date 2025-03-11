81395b3efca10d8779d7bf5808c2153ef473926b
hamilkarr <12uclock@gmail.com>
2025-03-11 18:13:43 +0900

    선언적 트랜잭션 적용용

--- src/test/java/theory/jdbc/connection/ConnectionTest.java
+++ src/test/java/theory/jdbc/connection/ConnectionTest.java
@@ -7,7 +7,6 @@ import static theory.jdbc.connection.ConnectionConst.USERNAME;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 import javax.sql.DataSource;
 
---
+++ src/test/java/theory/jdbc/service/MemberServiceV5Test.java
@@ -0,0 +1,124 @@
+package theory.jdbc.service;
+
+
+import static theory.jdbc.connection.ConnectionConst.PASSWORD;
+import static theory.jdbc.connection.ConnectionConst.URL;
+import static theory.jdbc.connection.ConnectionConst.USERNAME;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import lombok.extern.slf4j.Slf4j;
+import theory.jdbc.domain.Member;
+import theory.jdbc.repository.MemberRepositoryV3;
+
+/**     
+ * 기본 동작,  트랜잭션이 없어서 문제 발생
+ */
+@Slf4j
+@SpringBootTest
+public class MemberServiceV5Test {
+
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    @Autowired
+    private MemberRepositoryV3 memberRepository;
+    @Autowired
+    private MemberServiceV5 memberService;
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        DataSource dataSource() {
+            return new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        }
+
+        @Bean
+        PlatformTransactionManager transactionManager() {
+            return new DataSourceTransactionManager(dataSource());
+        }
+
+        @Bean
+        MemberRepositoryV3 memberRepositoryV3() {
+            return new MemberRepositoryV3(dataSource());
+        }
+
+        @Bean
+        MemberServiceV5 memberServiceV5() {
+            return new MemberServiceV5(memberRepositoryV3());
+        }
+    }
+
+    @AfterEach
+    void afterEach() throws SQLException {
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
+    }
+
+    @Test
+    @DisplayName("AOP CHECK")
+    void aopCheck() {
+        log.info("memberService class={}", memberService.getClass());
+        log.info("memberRepository class={}", memberRepository.getClass());
+        Assertions.assertThat(AopUtils.isAopProxy(memberService)).isTrue();
+    }
+
+    @Test
+    @DisplayName("정상 이체")
+    void testAccountTransfer() throws SQLException {
+        // given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        // when
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+
+        // then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberB = memberRepository.findById(memberB.getMemberId());
+        
+        Assertions.assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        Assertions.assertThat(findMemberB.getMoney()).isEqualTo(12000);
+    }
+
+    @Test
+    @DisplayName("예외 이체")
+    void testAccountTransferEx() throws SQLException {
+        // given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_EX, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        // when
+        Assertions.assertThatThrownBy(() -> memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000))
+            .isInstanceOf(IllegalStateException.class);
+
+        // then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberB = memberRepository.findById(memberB.getMemberId());
+        
+        Assertions.assertThat(findMemberA.getMoney()).isEqualTo(10000);
+        Assertions.assertThat(findMemberB.getMoney()).isEqualTo(10000);
+    }
+}
+
